2b9f62803196d8f966ca4b3029b5808e4b71a9d5
UBB <ubb@example.com>
2017-01-21 22:05:04 +0900

    Doing 'Homework:secure your website'

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from django.contrib.auth import views
 
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', views.login, name='login'),
+	url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
